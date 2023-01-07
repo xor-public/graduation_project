058a0c3ca31ba875d64d53d4e83ab0ee98fe78db
xc <xcandlrr@gmail.com>
2023-01-07 20:43:42 +0800

    fix aggregate bugs

--- server.py
+++ server.py
@@ -13,7 +13,8 @@ class Server:
         self.clients=self.clients_init()
         self.device=torch.device(config["device"])
         self.model=self.model_init()
-        self.next_model=self.model*(1-config["C"])
+        # self.next_model=self.model*(1-config["C"])
+        # self.next_model=self.next_model-self.next_model
     def load_data(self,val_data):
         self.val_loader=DataLoader(val_data,batch_size=self.config["B"])
         self.criterion=torch.nn.CrossEntropyLoss()
@@ -30,19 +31,27 @@ class Server:
         m=max(int(self.config["C"]*self.config["K"]),1)
         selected_clients=random.sample([i for i in range(self.config["K"])],m)
         return selected_clients
-    def aggregate_models(self,recieved):
+    def aggregate_models(self,recieved_models):
         # self.next_model+=(recieved-self.model)*(self.config["eta"]/(self.config["C"]*self.config["K"]))
-        self.next_model+=recieved*(1/self.config["K"])
+        # self.next_model+=recieved*(1/(self.config["K"]*self.config["C"]))
+        for key in self.model.state_dict().keys():
+            self.model.state_dict()[key]*=0
+        for model in recieved_models:
+            for key in model.state_dict().keys():
+                self.model.state_dict()[key]+=model.state_dict()[key]/len(recieved_models)
     def train_one_epoch(self):
         selected_clients=self.select_clients()
+        recieved_models=[]
         for client_idx in tqdm(selected_clients):
             client=self.clients[client_idx]
             client.get_model(self.model)
             client.train_model()
             recieved=client.submit_model()
-            self.aggregate_models(recieved)
-        self.model=copy.deepcopy(self.next_model)
-        self.next_model=copy.deepcopy(self.model)*(1-self.config["C"])
+            recieved_models.append(recieved)
+        # self.model=copy.deepcopy(self.next_model)
+        # self.next_model=copy.deepcopy(self.model)*(1-self.config["C"])
+        # self.next_model=self.next_model-self.next_model
+        self.aggregate_models(recieved_models)
     def validate(self):
         self.model.validate(self.val_loader,self.criterion)
 
